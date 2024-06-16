b3c6545b92e9f188ef685f6715793f7b0b3086e5
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-16 15:47:40 +0300

    Uygulama tamamlandı

--- app/src/main/java/com/example/ilhamverapp_jetpack/MainActivity.kt
+++ app/src/main/java/com/example/ilhamverapp_jetpack/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.example.ilhamverapp_jetpack
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ilhamverapp_jetpack.ui.theme.IlhamVerAppJetpackTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +41,64 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IlhamVerAppJetpackTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    Sayfa()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Sayfa() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "IlhamVerApp Jetpack")
+                }, colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.anaRenk),
+                    titleContentColor = colorResource(id = R.color.white)
+                )
+            )
+        },
+        content = {
+            Column(
+                modifier =
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(
+                        painter = painterResource(id = R.drawable.stevejobs),
+                        contentDescription = "Steve Jobs"
+                    )
+                    Text(
+                        text = "Steve Jobs",
+                        color = androidx.compose.ui.graphics.Color.Red,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 22.sp
+                    )
+                    Text(
+                        text = "Dünyayı değiştirecek insanlar, onu değiştireceklerini düşünecek kadar çılgın olanlardır.",
+                        modifier = Modifier.padding(all = 10.dp),
+                        textAlign = TextAlign.Center
+                    )
+                    Button(onClick = { Log.e("Button", "İlham verildi") },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor =  androidx.compose.ui.graphics.Color.Red,
+                            contentColor = colorResource(id = R.color.white)
+
+                        )) {
+                        Text(text = "İLHAM VER")
+                    }
+                }
+            }
+        }
     )
 }
 
@@ -42,6 +106,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     IlhamVerAppJetpackTheme {
-        Greeting("Android")
+        Sayfa()
     }
 }
--- app/src/main/java/com/example/ilhamverapp_jetpack/ui/theme/Theme.kt
+++ app/src/main/java/com/example/ilhamverapp_jetpack/ui/theme/Theme.kt
@@ -2,6 +2,8 @@ package com.example.ilhamverapp_jetpack.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import android.view.Window
+import android.view.WindowManager
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -9,7 +11,19 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import com.example.ilhamverapp_jetpack.R
+
+
+private fun setStatusBarColor(window: Window, color: Color) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = color.toArgb()
+    }
+}
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -49,7 +63,9 @@ fun IlhamVerAppJetpackTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
+    val context = LocalContext.current
+    val window: Window = (context as Activity).window
+    setStatusBarColor(window, colorResource(id = R.color.anaRenkKoyu))
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
